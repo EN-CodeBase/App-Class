3c0f039456cafda6803ec6520d5f9083418629dd
Erli Nuraj <erlinuraj@gmail.com>
2026-03-09 20:56:34 +0100

    Add files via upload
    Signed-off-by: Erli Nuraj <erlinuraj@gmail.com>

---
+++ APP-Name/AppImpl/Setup.cpp
@@ -0,0 +1,18 @@
+#include "../App.h"
+// INCLUDES
+#include <iostream>
+
+
+// ----------------------------------
+// Static member definitions
+std::atomic_bool App::running{false};
+std::thread App::PrivateThread;
+using namespace std;
+// ----------------------------------
+
+void App::Setup() {
+
+/* TODO:    Add App Setup Sequence Here   */
+
+
+}
